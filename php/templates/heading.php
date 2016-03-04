570e56a9b32dec50232aab12ac27e0e1fb6d02b5
Thiem Huynh <thiem.huynh0@gmail.com>
2016-03-04 14:29:24 -0500

    prioritized main.css, added carousel imgs, carousel css, media queries, centered content, commented out footer

--- php/templates/heading.php
+++ php/templates/heading.php
@@ -3,10 +3,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Donation website for the non profit corparation of Bethany Negash">
 <link rel="shortcut icon" type="image/x-icon" href="img/favicon.jpg" />
-<link rel="stylesheet" type="text/css" href="css/main.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
 <link rel="stylesheet" type="text/css" href="css/scroll.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
 <style type="text/css">
 #back-top span {
 background:#030303 url(img/up-arrow.png) no-repeat center center;
