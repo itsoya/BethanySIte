ff9de4c1fbcc90abe421b5cd458e3145ac868d4d
Thiem Huynh <thiem.huynh0@gmail.com>
2016-03-08 10:39:03 -0500

    setup about.php page, added about.css, modified main.css

--- about.php
+++ about.php
@@ -2,21 +2,41 @@
 <html lang="en">
 	<head>
 		<?php require 'php/templates/heading.php';?>
+		<link rel="stylesheet" type="text/css" href="css/about.css"
 		<title>Bethany homepage (testing)</title>
 	</head>
 	<body>
 		<?php require 'php/templates/nav.php';?>
 		<body data-spy="scroll" data-target="#my-navbar">
-			<div class="row container">
 
-			</div>
+			<div class="container main-wrapper">
+				<div class="row">
+					<div class="col-md-12">
+						<h1>About Us</h1>
+					</div>
+				</div> <!-- end row -->
+				<div class="row about-row-custom">
+					<div class="col-md-3 about-col-custom" style="background-color:salmon;">
+						<h2>About Bethany Negash</h2>
+						<img src="http://placehold.it/550x450" alt="Bethany Negash Picture" class="no-style-custom">
+					</div>
+					<div class="col-md-9 about-col-custom" style="background-color:lightgreen;">
+						<h2>Our Mission</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est mi, iaculis fringilla mi interdum, sodales faucibus ex. Aliquam eros eros, venenatis sit amet molestie nec, ultrices sed arcu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut non urna neque. Maecenas blandit leo ut risus lobortis euismod. Integer porttitor ante justo, vel ornare urna viverra eu. Nunc hendrerit auctor urna, quis tempus ante posuere non.Quisque faucibus vel elit sed eleifend. Aenean ornare mauris quis dolor dictum tempus. Nam vestibulum nisi ut felis porta fermentum. Ut convallis ullamcorper mauris, id laoreet neque luctus quis. Sed feugiat, sapien ut viverra viverra, enim tortor ultricies tortor, eget dapibus velit tellus vel nibh. Aliquam in dignissim sapien. Nullam ac purus in lorem imperdiet ultricies. Sed vitae mi elit. Vivamus libero felis, scelerisque a lobortis quis, faucibus et tortor. Sed eu velit pretium, pharetra est viverra, aliquet felis. Etiam ullamcorper mattis luctus.</p>
+						<h2>What we do</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est mi, iaculis fringilla mi interdum, sodales faucibus ex. Aliquam eros eros, venenatis sit amet molestie nec, ultrices sed arcu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut non urna neque. Maecenas blandit leo ut risus lobortis euismod. Integer porttitor ante justo, vel ornare urna viverra eu. Nunc hendrerit auctor urna, quis tempus ante posuere non.Quisque faucibus vel elit sed eleifend. Aenean ornare mauris quis dolor dictum tempus. Nam vestibulum nisi ut felis porta fermentum. Ut convallis ullamcorper mauris, id laoreet neque luctus quis. Sed feugiat, sapien ut viverra viverra, enim tortor ultricies tortor, eget dapibus velit tellus vel nibh. Aliquam in dignissim sapien. Nullam ac purus in lorem imperdiet ultricies. Sed vitae mi elit. Vivamus libero felis, scelerisque a lobortis quis, faucibus et tortor. Sed eu velit pretium, pharetra est viverra, aliquet felis. Etiam ullamcorper mattis luctus.</p>
+						<h2>Who we are</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est mi, iaculis fringilla mi interdum, sodales faucibus ex. Aliquam eros eros, venenatis sit amet molestie nec, ultrices sed arcu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut non urna neque. Maecenas blandit leo ut risus lobortis euismod. Integer porttitor ante justo, vel ornare urna viverra eu. Nunc hendrerit auctor urna, quis tempus ante posuere non.Quisque faucibus vel elit sed eleifend. Aenean ornare mauris quis dolor dictum tempus. Nam vestibulum nisi ut felis porta fermentum. Ut convallis ullamcorper mauris, id laoreet neque luctus quis. Sed feugiat, sapien ut viverra viverra, enim tortor ultricies tortor, eget dapibus velit tellus vel nibh. Aliquam in dignissim sapien. Nullam ac purus in lorem imperdiet ultricies. Sed vitae mi elit. Vivamus libero felis, scelerisque a lobortis quis, faucibus et tortor. Sed eu velit pretium, pharetra est viverra, aliquet felis. Etiam ullamcorper mattis luctus.</p>
+					</div>
+				</div> <!-- end row -->
+			</div> <!-- end container -->
 			<!--End of container fluid-->
 			<!--Top of page-->
 			<p id="back-top">
 				<a href="#top"><span></span></a>
 			</p>
 			<!--end of top of page-->
-			<?php require 'php/templates/footer.php';?>
+			<!-- ?php require 'php/templates/footer.php';? -->
 			<?php require 'php/templates/footing.php';?>
 		</body>
 	</html>
