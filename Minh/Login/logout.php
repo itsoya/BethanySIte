9907b6260f2e4503e53d51b94d0f6cbb0a068019
Itsoya Abebe <itsoyaa@gmail.com>
2016-03-16 18:41:22 -0400

    Revert "backend auth"

--- Minh/Login/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    setcookie("email", "", time() - 10000000);
-    setcookie("first_name", "", time() - 10000000);
-    setcookie("last_name", "", time() - 10000000);
-    setcookie("token", "", time() - 10000000);
- ?>
