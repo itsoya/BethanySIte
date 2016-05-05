8fe6c7844d2ee4c874c9d29d1b7e41118e194e38
Thiem Huynh <thiem.huynh0@gmail.com>
2016-05-05 10:36:00 -0400

    Minh fixed Facility visitor stat bug

--- Minh/get_stats.php
+++ Minh/get_stats.php
@@ -16,12 +16,23 @@
 
     $result = mysqli_query($db, $sql);
     $row = mysqli_fetch_assoc($result);
-    $total_donation = $row["total_donation"]; // This is the total donation
-    $current_year_total = $row["current_year_total"]; // This is the total donation this year
-    $total_visitor = $row["total_visitor"]; // This is the total visitors
-    $current_month_visitor = $row["current_month_visitor"]; // This is the total donation this year
-    $total_web_visitor = $row["total_web_visitor"]; // This is the total visitors
-    $current_year_web_visitor = $row["current_year_web_visitor"]; // This is the total donation this year
+    $total_donation = $row["total_donation"] == NULL ? 0 : $row["total_donation"]; // This is the total donation
+    $current_year_total = $row["current_year_total"] == NULL ? 0 : $row["current_year_total"]; // This is the total donation this year
+    $total_visitor = $row["total_visitor"] == NULL ? 0 : $row["total_visitor"]; // This is the total visitors
+    $current_month_visitor = $row["current_month_visitor"] == NULL ? 0 : $row["current_month_visitor"]; // This is the total donation this year
+    $total_web_visitor = $row["total_web_visitor"] == NULL ? 0 : $row["total_web_visitor"]; // This is the total visitors
+    $current_year_web_visitor = $row["current_year_web_visitor"] == NULL ? 0 : $row["current_year_web_visitor"]; // This is the total donation this year
 
-    print_r(json_encode($row));
+    $data = array("total_donation" => $total_donation,
+                    "current_year_total" => $current_year_total,
+                    "total_visitor" => $total_visitor,
+                    "current_month_visitor" => $current_month_visitor,
+                    "total_web_visitor" => $total_web_visitor,
+                    "current_year_web_visitor" => $current_year_web_visitor);
+    //echo "$current_year_total $total_donation $current_month_visitor $total_visitor $total_web_visitor $current_year_web_visitor";
+    print_r(json_encode($data));
+    /*
+    echo "total donation: $total_donation <br>total donation this year: $current_year_total
+        <br>total visitors: $total_visitor <br>total visitor this month: $current_month_visitor";
+        */
 ?>
